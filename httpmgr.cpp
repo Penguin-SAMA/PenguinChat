9b610b0fad9366fb5f03de50c7775c2b91da34f0
Penguin-SAMA <496107146@qq.com>
2025-02-25 16:03:59 +0800

    add commit

--- httpmgr.cpp
+++ httpmgr.cpp
@@ -3,16 +3,21 @@
 HttpMgr::~HttpMgr() {}
 
 HttpMgr::HttpMgr() {
+    // 连接 http 请求和完成信号，信号槽机制保证队列消费
     connect(this, &HttpMgr::sig_http_finish, this, &HttpMgr::slot_http_finish);
 }
 
 void HttpMgr::PostHttpReq(QUrl url, QJsonObject json, ReqId req_id, Modules mod) {
+    // 创建 HTTP POST 请求，并设置请求头和请求体
     QByteArray data = QJsonDocument(json).toJson();
+    // 通过 url 构造请求
     QNetworkRequest request(url);
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
     request.setHeader(QNetworkRequest::ContentLengthHeader, QByteArray::number(data.length()));
+    // 发送请求，处理响应，获取自己的智能指针，构造伪闭包并增加智能指针引用计数
     auto self = shared_from_this();
     QNetworkReply* reply = _manager.post(request, data);
+    // 设置信号和槽等待发送完成
     QObject::connect(reply, &QNetworkReply::finished, [self, reply, req_id, mod]() {
         // 处理错误
         if (reply->error() != QNetworkReply::NoError) {
@@ -23,8 +28,9 @@ void HttpMgr::PostHttpReq(QUrl url, QJsonObject json, ReqId req_id, Modules mod)
             return;
         }
 
-        // 无错误
+        // 无错误则读回请求
         QString res = reply->readAll();
+
         // 发送信号通知完成
         emit self->sig_http_finish(req_id, res, ErrorCodes::SUCCESS, mod);
         reply->deleteLater();
